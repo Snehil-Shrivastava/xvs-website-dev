67fee964adb03e7c745835365fd7622b2cb359ef
Juhi2116 <69980611+Juhi2116@users.noreply.github.com>
2025-08-19 17:03:07 +0530

    adding more font weights

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ const roboto = Roboto({
 
 const poppins = Poppins({
   subsets: ["latin"],
-  weight: ["300", "400", "500", "600", "700"],
+  weight: ["100", "200", "300", "400", "500", "600", "700"],
 });
 
 export const metadata: Metadata = {
