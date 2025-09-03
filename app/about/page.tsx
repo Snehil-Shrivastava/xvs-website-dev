5e6451a2097102a8da47a4598c26d2e87673b11c
Snehil-Shrivastava <snehil.shrivastava@xvscreations.com>
2025-09-03 17:04:18 +0530

    about page svg scrolltrigger debug

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,7 +6,6 @@ import RichText from "../components/RichText/RichText";
 import SectionHeadingText from "../components/SectionHeadingText/SectionHeadingText";
 import styles from "./page.module.css";
 
-
 import * as motion from "motion/react-client";
 
 import GridSVG from "../components/GridSVG/GridSVG";
@@ -111,6 +110,9 @@ const AboutPage = () => {
           // @ts-ignore
           const { isMobile, isTablet, isMobilesm } = context.conditions;
 
+          const finalScale = isTablet ? 0.9 : isMobile ? 0.8 : 0.6; // Define final scale for calculation
+          const finalX = isMobile ? -30 : isTablet ? -60 : -70;
+
           gsap.to(svgRef.current, {
             scrollTrigger: {
               trigger: svgRef.current,
@@ -128,30 +130,63 @@ const AboutPage = () => {
               },
               scrub: 1,
             },
-            scale: isTablet ? 0.9 : 0.6,
-            x: isMobile ? -30 : isTablet ? -60 : -70,
+            // scale: isTablet ? 0.9 : 0.6,
+            scale: finalScale,
+            x: isMobile ? -40 : isTablet ? -60 : -70,
             // y: isMobilesm ? 250 : isMobile ? 378 : 555,
             y: () => {
-              // @ts-ignore
-              const topSection = topSectionRef.current.offsetHeight;
-              // @ts-ignore
-              const container = containerRef.current.offsetTop;
-              // @ts-ignore
-              const richTxt = richTxtRef.current.offsetHeight;
-              // @ts-ignore
-              const richTxtSVG = richTxtRef.current.firstChild.clientHeight;
-              // @ts-ignore
-              const richTxtPara = richTxtRef.current.lastChild.offsetHeight;
+              // // @ts-ignore
+              // const topSection = topSectionRef.current.offsetHeight;
+              // // @ts-ignore
+              // const container = containerRef.current.offsetTop;
+              // // @ts-ignore
+              // const richTxt = richTxtRef.current.offsetHeight;
+              // // @ts-ignore
+              // const richTxtSVG = richTxtRef.current.firstChild.clientHeight;
+              // // @ts-ignore
+              // const richTxtPara = richTxtRef.current.lastChild.offsetHeight;
 
-              console.log(richTxtSVG);
+              // console.log(richTxtSVG);
 
-              const translate = topSection - container;
+              // const translate = topSection - container;
 
-              console.log("translate", translate + richTxtSVG / 5);
+              // console.log("translate", translate + richTxtSVG / 5);
 
-              return isMobile
-                ? translate + richTxtSVG / 5
-                : translate + richTxtSVG / 5 + 72;
+              // return isMobile
+              //   ? translate + richTxtSVG / 5
+              //   : translate + richTxtSVG / 5 + 72;
+
+              const containerRect =
+                // @ts-ignore
+                containerRef.current.getBoundingClientRect();
+              // @ts-ignore
+              const svgRect = svgRef.current.getBoundingClientRect();
+              // @ts-ignore
+              const richTxtRect = richTxtRef.current.getBoundingClientRect();
+
+              // 1. Find the bottom of the rich text relative to the pinned container
+              const richTxtBottom = richTxtRect.bottom - containerRect.top; // 566 - 160
+
+              // 2. Calculate the height of the SVG *at its final scale*
+              const svgFinalHeight = svgRect.height * finalScale; // 98 * 0.8
+
+              // 3. The final top position of the SVG should be the rich text bottom minus the SVG's final height
+              const finalTopPosition = richTxtBottom - svgFinalHeight; // 406 - 78.4
+
+              // 4. The required translation is the difference between where it needs to be and where it currently is
+              const currentTopPosition = svgRect.top - containerRect.top; // 160 - 160
+
+              console.log("finalScale", finalScale);
+
+              console.log("containerRect", containerRect);
+              console.log("svgRect", svgRect);
+              console.log("richTxtRect", richTxtRect);
+              console.log("richTxtBottom", richTxtBottom);
+              console.log("svgFinalHeight", svgFinalHeight);
+              console.log("finalTopPosition", finalTopPosition);
+              console.log("currentTopPosition", currentTopPosition);
+
+              return finalTopPosition - currentTopPosition;
             },
           });
         }
@@ -182,7 +217,7 @@ const AboutPage = () => {
         </h1>
         <div
           ref={containerRef}
-          className={`absolute left-1/2 -translate-x-1/2 max-sm:top-40 pointer-events-none max-sm:h-24 max-sm:w-21 sm:max-lg:top-56.5 sm:max-lg:w-29 sm:max-lg:h-33.75 lg:max-xl:top-80 lg:max-2xl:w-51 lg:max-2xl:h-59.25 xl:max-2xl:top-95 min-[1536px]:max-[1906px]:w-80 min-[1536px]:max-[1906px]:h-100 min-[1536px]:max-[1906px]:top-100 min-[1906px]:w-110 min-[1906px]:h-130 min-[1906px]:top-120`}
+          className={`absolute left-1/2 -translate-x-1/2 max-sm:top-40 pointer-events-none max-sm:h-24 max-sm:w-21 sm:max-lg:top-56.5 sm:max-lg:w-29 sm:max-lg:h-33.75 lg:max-xl:top-80 lg:max-2xl:w-51 lg:max-2xl:h-59.25 xl:max-2xl:top-95 min-[1536px]:max-[1906px]:w-80 min-[1536px]:max-[1906px]:h-100 min-[1536px]:max-[1906px]:top-100 min-[1906px]:w-110 min-[1906px]:h-130 min-[1906px]:top-120 border border-blue-500`}
         >
           <div className="relative h-full">
             <div className="absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2">
@@ -240,15 +275,18 @@ const AboutPage = () => {
                 </defs>
               </svg>
             </div>
-            <div className="absolute">
+            <div
+              ref={svgRef}
+              className={`absolute ${styles.svgRefDebug} border border-amber-600`}
+            >
               <svg
-                ref={svgRef}
+                // ref={svgRef}
                 xmlns="http://www.w3.org/2000/svg"
                 width="268"
                 height="287"
                 viewBox="-38 0 268 287"
                 fill="none"
-                className="max-sm:w-21 max-sm:h-24 sm:max-lg:w-29 sm:max-lg:h-33.75 lg:max-2xl:w-51 lg:max-2xl:h-59.25 min-[1536px]:max-[1906px]:w-80 min-[1536px]:max-[1906px]:h-100 min-[1906px]:w-110 min-[1906px]:h-130"
+                className={`max-sm:w-21 max-sm:h-24 sm:max-lg:w-29 sm:max-lg:h-33.75 lg:max-2xl:w-51 lg:max-2xl:h-59.25 min-[1536px]:max-[1906px]:w-80 min-[1536px]:max-[1906px]:h-100 min-[1906px]:w-110 min-[1906px]:h-130`}
               >
                 <foreignObject
                   x="-29.3574"
@@ -449,11 +487,43 @@ const AboutPage = () => {
         </div>
       </div>
       <div
-        ref={richTxtRef}
-        className="relative pt-[72px] max-[768px]:pt-0"
-        style={{ opacity: 0 }}
+        // ref={richTxtRef}
+        className="relative pt-[72px] max-[768px]:pt-0 border border-red-500"
+        // style={{ opacity: 0 }}
       >
-        <RichText />
+        {/* <RichText /> */}
+        <svg
+          ref={richTxtRef}
+          xmlns="http://www.w3.org/2000/svg"
+          width="217"
+          height="172"
+          viewBox="0 0 217 172"
+          fill="none"
+          className="w-auto max-sm:h-20 sm:max-lg:h-35.5 lg:max-xl:h-43 min-[1906px]:h-70 mx-auto scale-[1] translate-x-[5px] border border-emerald-500"
+        >
+          <path
+            d="M207.327 93.0358C204.169 89.0617 200.288 85.6712 195.779 82.6053C193.841 81.2861 191.782 80.0244 189.625 78.7997C186.107 76.8065 182.309 74.916 178.263 73.0297C162.077 65.5829 154.827 62.217 148.785 55.7319C146.102 52.6208 144.664 49.0947 144.461 45.1617C144.445 44.8452 144.432 44.5247 144.432 44.2C144.432 35.5532 152.166 28.8256 161.585 28.8256C162.933 28.8256 164.23 28.9201 165.474 29.1009C170.108 29.7872 174.018 31.7517 177.209 34.9655L183.115 24.6131L192.353 8.42081C185.078 3.41928 176.155 0.653445 165.59 0.106853C164.28 0.0369874 162.949 0 161.589 0C141.899 0 126.457 9.77291 119.129 24.6172C116.116 30.7201 114.475 37.6779 114.475 45.1658C114.475 56.8374 117.769 66.1212 123.564 73.6913C123.944 74.1886 124.337 74.6777 124.738 75.1626C125.544 76.1284 126.391 77.0654 127.28 77.9778C128.135 78.8531 129.028 79.6997 129.954 80.5258C130.57 81.0724 131.202 81.6066 131.847 82.1368C132.76 82.8807 133.703 83.6122 134.678 84.3191C135.12 84.6396 135.567 84.9561 136.017 85.2684C137.828 86.5178 139.733 87.7055 141.729 88.8356C142.229 89.1192 142.734 89.3987 143.242 89.674C144.259 90.2288 145.3 90.7672 146.363 91.2932L150.545 93.3029L153.368 94.6591C154.617 95.2715 155.815 95.855 156.956 96.4181C157.336 96.6071 157.712 96.7921 158.08 96.9729C159.188 97.5236 160.246 98.0496 161.259 98.5592C162.156 99.0113 163.011 99.451 163.838 99.8784C164.106 100.018 164.375 100.158 164.635 100.294C165.218 100.598 165.784 100.902 166.334 101.198C166.644 101.366 166.954 101.531 167.252 101.699C168.041 102.131 168.793 102.558 169.513 102.977C169.81 103.15 170.104 103.322 170.393 103.495C170.583 103.61 170.773 103.721 170.959 103.836C171.199 103.98 171.43 104.124 171.662 104.272C172.261 104.65 172.844 105.024 173.402 105.398C173.675 105.583 173.943 105.768 174.208 105.953C174.505 106.162 174.799 106.372 175.084 106.586C175.444 106.849 175.791 107.116 176.13 107.387C177.151 108.192 178.101 109.023 178.998 109.902C179.25 110.149 179.498 110.399 179.746 110.654C179.986 110.901 180.222 111.156 180.453 111.41C180.685 111.665 180.916 111.928 181.143 112.191C183.801 115.315 185.252 119.157 185.252 123.242C185.252 130.845 182.462 136.475 177.647 139.792C173.939 142.344 169.033 143.524 163.267 143.178C158.155 142.87 153.699 142.151 149.768 140.856L135.604 165.757C143.569 169.735 152.674 172.004 162.54 172.004C194.192 172.004 216.66 152.306 216.66 121.796C216.329 109.253 212.998 100.191 207.311 93.0317L207.327 93.0358Z"
+            fill="#F3EDDE"
+          />
+          <path
+            d="M41.4123 55.7319L53.5102 34.723C42.4167 15.095 33.8941 0 33.8941 0H0.758301C12.8685 21.202 24.9292 42.3136 36.9857 63.417L41.4123 55.7319Z"
+            fill="#F3EDDE"
+          />
+          <path
+            d="M122.51 100.918C118.691 98.1518 115.227 95.1558 112.148 91.9667C107.325 100.424 102.758 108.43 99.0004 115.006C92.7304 104.029 81.2691 83.8048 69.692 63.3384L53.2998 91.9749C68.4851 118.557 83.6952 145.179 99.0211 172.007C111.135 150.797 123.171 129.723 135.186 108.681C130.566 106.277 126.313 103.671 122.51 100.918Z"
+            fill="#F3EDDE"
+          />
+        </svg>
+        <p
+          // @ts-ignore
+          className="font-light text-center w-4/5 mx-auto text-[#F3EDDE] text-[0.5rem]/[1rem] sm:max-lg:text-[1rem]/[2rem] lg:max-xl:text-[1rem]/[2rem] lg:max-xl:w-7/10 lg:max-xl:mt-15 xl:max-2xl:text-[1.25rem]/[2.5rem] xl:max-2xl:mt-20 xl:max-2xl:w-3/5 min-[1536px]:max-[1906px]:text-[1.75rem]/[3rem] min-[1536px]:max-[1906px]:w-3/5 min-[1536px]:max-[1906px]:mt-20 min-[1906px]:text-[2rem]/[3.5rem] min-[1906px]:w-3/5 min-[1906px]:mt-25 mt-[22px]"
+        >
+          With the inspiration of designing we started back in 2014 and bringing
+          them to reality since then. From cosmetic startups to big automotive
+          companies we had fun working at different scales and industries. We
+          also picked a couple of awards along the way but customer satisfaction
+          is what really mattered.
+        </p>
       </div>
       <div className="text-center my-[200px] w-[80%] mx-auto max-[1025px]:w-[85%] max-[769px]:w-[90%] max-[426px]:my-[112px]">
         <SectionHeadingText
